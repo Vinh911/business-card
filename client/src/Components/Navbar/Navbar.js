ca055947c49904b2ba1dff4c0587a92dde45169e
Vinh <nguyenvinh1997@yahoo.de>
2022-08-05 00:44:34 +0200

    basic navbar

---
+++ client/src/Components/Navbar/Navbar.js
@@ -0,0 +1,34 @@
+import './Navbar.css';
+import { useState } from 'react';
+
+
+
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const displayMenue = () => {
+        if (isOpen) {
+            return (
+                <>
+                    <ul className="navbar-list">
+                        <li>Einstellungen</li>
+                        <li>Datenschutz</li>
+                        <li>Logout</li>
+                    </ul>
+                    <br style={{ clear: "both" }}></br>
+                </>
+            )
+        }
+    }
+
+    return (
+        <div className="navbar">
+            {isOpen ? displayMenue() : null}
+
+            <div className="navbar-items">
+                <button onClick={() => setIsOpen(!isOpen)}>Menue</button>
+                <button>Add</button>
+            </div>
+        </div >
+    );
+}
