ce29f5fdb28a20ed6285a8063856e3862d6fc562
Vinh <nguyenvinh1997@yahoo.de>
2022-08-06 01:16:17 +0200

    display card

--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -1,19 +1,57 @@
 import './Profile.css';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 function Profile() {
-    return (
-        <div className="Profile">
-            <div className="top">
-                TOP
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [data, setData] = useState();
+    const [message, setMessage] = useState();
+
+    let id = searchParams.get('id');
+
+    useEffect(() => {
+
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        var url = "https://bc.bytebro.de/api/displayCard.php?id=" + id;
+
+        fetch(url, requestOptions)
+            .then(response => response.text())
+            .then(result => setData(JSON.parse(result)))
+            .catch(error => setMessage(error));
+    }, [id]);
+
+    if (!data) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div className="profile">
+                <div className='top'>
+                    <div className='data'>
+                        <p>{data['name']}</p>
+                        <p>{data['position']}</p>
+                        <p>{data['company']}</p>
+                    </div>
+                </div>
+                <div className='middle'>
+                    <div className='data'>
+                        {message ? <p>{message}</p> : null}
+                    </div>
+                </div>
+                <div className='bottom'>
+                    <div className='data'>
+                        <p>{data['phone']}</p>
+                        <p>{data['email']}</p>
+                        <p>{data['website']}</p>
+                        <p>{data['address']}</p>
+                    </div>
+                </div>
             </div>
-            <div className="middle">
-                MIDDLE
-            </div>
-            <div className="bottom">
-                BOTTOM
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Profile;
