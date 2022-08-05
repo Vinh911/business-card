dfdbccb82820e3cc26a2dd08fc0047b73755150b
Vinh <nguyenvinh1997@yahoo.de>
2022-08-05 23:44:53 +0200

    api to get card data

---
+++ server/displayCard.php
@@ -0,0 +1,41 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    // header("Access-Control-Allow-Origin: http://localhost:3000");
+    header('Content-Type: application/json');
+    header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Allow-Methods: POST");
+    include 'connection.php';
+    
+    $postData = json_decode(file_get_contents('php://input'), true);
+    $error = false;
+
+    if(!$postData['id']) {
+        $error = true;
+        echo json_encode('Etwas ist schief gelaufen');
+    }
+
+    if(!$error) {
+        $id = $postData['id'];
+        $statement = $pdo->prepare("SELECT * FROM profile WHERE id = :id");
+        $statement->execute(array("id" => $id));
+        $res = $statement->fetchAll();
+        if (count($res) > 0) {
+            $result = array(
+                    "id" => "894707",
+                    "user"=> 10,
+                    "color"=> null,
+                    "name"=> "Vinh Nguyen",
+                    "position"=> "Bürgermeister",
+                    "company"=> "Landeshauptstadt Dresden",
+                    "phone"=> "+49 173 8168686",
+                    "email"=> "vinh@dresden.de",
+                    "website"=> "www.dresden.de",
+                    "address"=> null,
+            );
+            echo json_encode($result);
+        } else {
+            $error = true;
+            echo json_encode('Etwas ist schief gelaufen');
+        }
+    }
+?>
